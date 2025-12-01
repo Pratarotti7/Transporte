9582dfdf6dc271f9866e63d01380894bba4b4db5
ROBSON PRATAROTTI <pratarotti7@gmail.com>
2025-12-01 19:58:50 -0300

    Add additional select queries to Select.sql

--- Select.sql
+++ Select.sql
@@ -1,2 +1,6 @@
 select * from marcamodelo 
 select * from veiculo
+select * from veiculo where id_veiculo = 1
+select * from veiculo where placa = 'TAT1A11'
+select top 1 * from veiculo
+select * from veiculo order by 1 desc
