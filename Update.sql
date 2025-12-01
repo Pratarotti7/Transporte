a252a098f6a685ca73eac416c2e55eda79dbbd5c
ROBSON PRATAROTTI <pratarotti7@gmail.com>
2025-12-01 20:10:14 -0300

    Update Update.sql

--- Update.sql
+++ Update.sql
@@ -1,1 +1,3 @@
 update veiculo set placa = 'OKC7R23' where id_veiculo = 1
+update transportadora set nome = 'Cart' where email = 'asasas@asasas.com'
+update motorista set categoria = 'D' where cpf = '32032032000'
